43d75d7bec5f0951190fdb467b13a48223561671
EDandanell <edandanell@gmail.com>
2014-03-19 16:53:01 -0700

    updated utilities and psxcontroller

--- psxcontroler/psxcontroler.ino
+++ psxcontroler/psxcontroler.ino
@@ -14,20 +14,62 @@ const int cmndPin  = 3;
 const int attPin   = 5;
 const int clockPin = 4;
 const int psxDelay = 50;         // determine the clock delay in microseconds
-
+const int handshake = 13;
+const int tx = 9;
+const int rx = 10;
 unsigned int data = 0;           // data stores the controller response
 
+const unsigned char  UP	= 1;  
+const unsigned char  DOWN =2; 
+const unsigned char  LEFT =3; 
+const unsigned char  RIGHT =4; 
+const unsigned char  UPRIGHT =5; 
+const unsigned char  UPLEFt =6; 
+const unsigned char  DOWNRIGHT =7; 
+const unsigned char  DOWNLEFT  =8; 
+const unsigned char  R1	=9; 
+const unsigned char  R2	=10; 
+const unsigned char  X	=11; 
+const unsigned char  TRIANGLE =12; 
+const unsigned char  SQUARE =13; 
+const unsigned char  CIRCLE =14; 
+const unsigned char  START =15; 
+const unsigned char  SELECT =16;
 
 
 void setup()
 {
+  pinMode(tx, OUTPUT);
+  digitalWrite( tx, LOW);
   // initialize the Psx library
-  pinMode(11, OUTPUT);
-  digitalWrite(11, LOW);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
   Psx.setupPins(dataPin, cmndPin, attPin, clockPin, psxDelay);
   Serial.begin(9600); // results will be displayed on the Serial Monitor
+}
+
+void bitbanging (unsigned char psxbutton) {
+
+  int j;
+
+digitalWrite( handshake, HIGH);
+digitalWrite( tx, LOW);
+delay(50);
+
+for (j=0; j<8; j++)
+{
+ if(psxbutton&(0x01<<j))
+   digitalWrite( tx, LOW);
+ else  
+   digitalWrite( tx, HIGH);
+delay(50);
+}
+
+digitalWrite( tx, HIGH);
+//Send the stop bits
+delay(50);
+delay(50);
+
+digitalWrite( handshake, LOW);
+  
 }
 
 
@@ -36,41 +78,65 @@ void loop()
   data = Psx.read();        // get the psx controller button data
 
   // check the button bits to see if a button is pressed
-  if(data & 1)
-    Serial.println("left button");
-  if(data & 2)
-    Serial.println("down button");
-  if(data & 4)
-    Serial.println("right button");
-    
-  if(data & 8) {
-    Serial.println("up button");
-    digitalWrite(11, HIGH);
-    digitalWrite(13, HIGH);
-  }
-  else {
-    digitalWrite(11, LOW);
-    digitalWrite(13, LOW);
+  if(data & 1) { //leftbutton
+    Serial.println(3);
+    bitbanging(LEFT);
   }
   
-  if(data & 16)
-    Serial.println("start button");
-  if(data & 128)
-    Serial.println("select button");
+  if(data & 2) {//down button
+    Serial.println(2);
+    bitbanging(DOWN);
+  }
   
-  if(data & 2048)
-    Serial.println("triangle button");
-  if(data & 512)
-    Serial.println("x button");
-  if(data & 1024)
-    Serial.println("circle button");
-  if(data & 256)
-    Serial.println("square button");
-    
-   if(data & 45056)
-    Serial.println("R1 button");
-   if(data & 16384)
-    Serial.println("R2 button");
-
+  if(data & 4) { //"right button"
+    Serial.println(4);
+      bitbanging(RIGHT);
+  }  
+  
+  if(data & 8) {//up button
+    Serial.println(1);
+       bitbanging(UP);
+  } 
+  
+  if(data & 16) {//start button
+    Serial.println(15);
+       bitbanging(START);
+  } 
+  
+  if(data & 128) {//select button
+    Serial.println(16);
+      bitbanging(DOWN);
+  }
+  
+  if(data & 2048) {//triangle button
+    Serial.println(12);
+        bitbanging(DOWN);
+  }
+  
+  if(data & 512) {//x button
+    Serial.println(11);
+       bitbanging(X);
+  } 
+  
+  if(data & 1024){//circle button
+    Serial.println(14);
+       bitbanging(CIRCLE);
+  } 
+  
+  if(data & 256) {//square button
+    Serial.println(13);
+        bitbanging(SQUARE);
+  }
+  
+   if(data & 45056) {//r1 button
+    Serial.println(9);
+       bitbanging(R1);
+  } 
+  
+   if(data & 16384) {//r2 button
+    Serial.println(10);
+    bitbanging(R2);
+  }
+  
   delay(100);
 }
--- utilities.c
+++ utilities.c
@@ -4,12 +4,14 @@ void wait_bit_time (void)
 	_asm
 	;For a 22.1184MHz crystal one machine cycle 
 	;takes 12/22.1184MHz=0.5425347us
-	mov R2, #2
-	N3: mov R1, #250
-	N2: mov R0, #184
-	N1: djnz R0, N1 ; 2 machine cycles-> 2*0.5425347us*184=200us
+	N2:
+	 mov R1, #250
+	N1:
+	 mov R0, #184
+	 
+	djnz R0, N1 ; 2 machine cycles-> 2*0.5425347us*184=200us
 	djnz R1, N2 ; 200us*250=0.05s
-	djnz R2, N3 ; 0.05s*20=1s
+
 	ret
 	_endasm;
 }
@@ -18,12 +20,17 @@ void wait_one_and_half_bit_time (void)
 	_asm
 	;For a 22.1184MHz crystal one machine cycle 
 	;takes 12/22.1184MHz=0.5425347us
-	mov R2, #3
-	M3: mov R1, #250
-	M2: mov R0, #184
-	M1: djnz R0, N1 ; 2 machine cycles-> 2*0.5425347us*184=200us
-	djnz R1, N2 ; 200us*250=0.05s
-	djnz R2, N3 ; 0.05s*20=1s
+	N3:
+	 mov R2, #3
+	N2:
+	 mov R1, #125
+	N1:
+	 mov R0, #184
+	 
+	djnz R0, N1 ; 2 machine cycles-> 2*0.5425347us*184=200us
+	djnz R1, N2 ; 200us*125=0.025s
+	DJNZ R2, N3 ; 3*0.025s= 0.075s
+
 	ret
 	_endasm;
 }
