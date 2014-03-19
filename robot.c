6cc0ea7ede816a346d1e5813229ad7b839a4ddc6
esecules <esecules@gmail.com>
2014-03-19 14:15:58 -0700

    Moved wait time to utilities

--- robot.c
+++ robot.c
@@ -123,34 +123,7 @@ void pwmcounter (void) interrupt 1
 		}
 	}
 }
-void wait_bit_time (void)
-{
-	_asm
-	;For a 22.1184MHz crystal one machine cycle 
-	;takes 12/22.1184MHz=0.5425347us
-	mov R2, #2
-	N3: mov R1, #250
-	N2: mov R0, #184
-	N1: djnz R0, N1 ; 2 machine cycles-> 2*0.5425347us*184=200us
-	djnz R1, N2 ; 200us*250=0.05s
-	djnz R2, N3 ; 0.05s*20=1s
-	ret
-	_endasm;
-}
-void wait_one_and_half_bit_time (void)
-{
-	_asm
-	;For a 22.1184MHz crystal one machine cycle 
-	;takes 12/22.1184MHz=0.5425347us
-	mov R2, #3
-	M3: mov R1, #250
-	M2: mov R0, #184
-	M1: djnz R0, N1 ; 2 machine cycles-> 2*0.5425347us*184=200us
-	djnz R1, N2 ; 200us*250=0.05s
-	djnz R2, N3 ; 0.05s*20=1s
-	ret
-	_endasm;
-}
+
 void SPIWrite( unsigned char value) 
 { 
 	SPSTA&=(~SPIF); // Clear the SPIF flag in SPSTA 
--- utilities.c
+++ utilities.c
@@ -1,9 +1,29 @@
 //Utilities file for functions like wait
-
-void wait_bit_time(){
-
+void wait_bit_time (void)
+{
+	_asm
+	;For a 22.1184MHz crystal one machine cycle 
+	;takes 12/22.1184MHz=0.5425347us
+	mov R2, #2
+	N3: mov R1, #250
+	N2: mov R0, #184
+	N1: djnz R0, N1 ; 2 machine cycles-> 2*0.5425347us*184=200us
+	djnz R1, N2 ; 200us*250=0.05s
+	djnz R2, N3 ; 0.05s*20=1s
+	ret
+	_endasm;
 }
-
-void wait_one_and_half_bit_time(){
-
+void wait_one_and_half_bit_time (void)
+{
+	_asm
+	;For a 22.1184MHz crystal one machine cycle 
+	;takes 12/22.1184MHz=0.5425347us
+	mov R2, #3
+	M3: mov R1, #250
+	M2: mov R0, #184
+	M1: djnz R0, N1 ; 2 machine cycles-> 2*0.5425347us*184=200us
+	djnz R1, N2 ; 200us*250=0.05s
+	djnz R2, N3 ; 0.05s*20=1s
+	ret
+	_endasm;
 }
