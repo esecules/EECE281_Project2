bb8079525213dedf363ccea7a9b54a57f25bcc61
EDandanell <edandanell@gmail.com>
2014-03-20 02:58:20 -0700

    updated utilities.c
    fixed the wait functions

--- utilities.c
+++ utilities.c
@@ -4,13 +4,13 @@ void wait_bit_time (void)
 	_asm
 	;For a 22.1184MHz crystal one machine cycle 
 	;takes 12/22.1184MHz=0.5425347us
-	N2:
+	
 	 mov R1, #250
-	N1:
+	N2:
 	 mov R0, #184
-	 
-	djnz R0, N1 ; 2 machine cycles-> 2*0.5425347us*184=200us
-	djnz R1, N2 ; 200us*250=0.05s
+	N1:
+	djnz R0, N1 ; //2 machine cycles-> 2*0.5425347us*184=200us
+	djnz R1, N2 ; //200us*250=0.05s
 
 	ret
 	_endasm;
@@ -20,16 +20,16 @@ void wait_one_and_half_bit_time (void)
 	_asm
 	;For a 22.1184MHz crystal one machine cycle 
 	;takes 12/22.1184MHz=0.5425347us
-	N3:
+
 	 mov R2, #3
-	N2:
+	M3:
 	 mov R1, #125
-	N1:
+	M2:
 	 mov R0, #184
-	 
-	djnz R0, N1 ; 2 machine cycles-> 2*0.5425347us*184=200us
-	djnz R1, N2 ; 200us*125=0.025s
-	DJNZ R2, N3 ; 3*0.025s= 0.075s
+	M1: 
+	djnz R0, M1 ; 2 machine cycles-> 2*0.5425347us*184=200us
+	djnz R1, M2 ; 200us*125=0.025s
+	DJNZ R2, M3 ; 3*0.025s= 0.075s
 
 	ret
 	_endasm;
