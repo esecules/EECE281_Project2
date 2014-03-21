0239224fe2e4426a7b1e1c29a292439e8b505035
jcj83429 <jcj83429@gmail.com>
2014-03-21 14:28:34 -0700

    moved adc code out

---
+++ SPI_ADC.h
@@ -0,0 +1,16 @@
+//         LP51B    MCP3004
+//---------------------------
+// MISO  -  P1.5  - pin 10
+// SCK   -  P1.6  - pin 11
+// MOSI  -  P1.7  - pin 9
+// CE*   -  P1.4  - pin 8
+// 4.8V  -  VCC   - pins 13, 14
+// 0V    -  GND   - pins 7, 12
+// CH0   -        - pin 1
+// CH1   -        - pin 2
+// CH2   -        - pin 3
+// CH3   -        - pin 4
+
+void SPIWrite(unsigned char value);
+unsigned int GetADC(unsigned char channel);
+float voltage (unsigned char channel);
